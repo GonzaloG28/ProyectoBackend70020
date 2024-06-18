e5d08a921df7eb86994d03f1b9cbeaf57ee3ab9e
GonzaloG28 <gonzalogimenez280@gmail.com>
2024-06-18 14:12:23 -0400

    funcion para crear carrito, obtener carrito por id, agregar producto al carrito

--- src/managers/cartManager.js
+++ src/managers/cartManager.js
@@ -1,3 +1,76 @@
+import fs from "fs"
+import { v4 as uuid } from "uuid"
 
+//iniciamos carts vacio
+let carts = []
+const path = "./src/managers/data/carts.json"
+
+
+//crear carrito
+const createCart = async() =>{
+    await getCarts()
+    const newCart = {
+        id: uuid(),
+        products: []
+    }
+
+    carts.push(newCart)
+    await fs.promises.writeFile(path, JSON.stringify(carts))
+
+    return newCart
+}
+
+
+//leer carritos
+const getCarts = async () =>{
+    const cartsJson = await fs.promises.readFile(path, "utf-8")
+    const parseFile = JSON.parse(cartsJson)
+    carts = parseFile || []
+
+    return carts
+}
+
+
+//leer carrito por ID
+const getCartById = async(cid) =>{
+    await getCarts()
+    const cart = carts.find((c) => c.id === cid)
+
+    return cart
+}
+
+
+//añadir producto al carrito
+const addProductToCart = async (cid, pid) =>{
+    await getCarts()
+    const cart = await getCartById(cid)
+
+    //logica para hacer que si producto ya esta, solo se sume quantity
+    const ProductExist = cart.products.findIndex(item => item.product === pid)
+
+    if(ProductExist !== -1){
+
+        cart.products[ProductExist].quantity++
+
+    } else{
+
+        const product = {
+            product: pid,
+            quantity: 1
+        }
+        cart.products.push(product)
+    }
+
+    await fs.promises.writeFile(path, JSON.stringify(carts))
+
+    return cart
+}
+
+export default {
+    getCarts,
+    createCart,
+    getCartById,
+    addProductToCart
+}
 
 
---
+++ src/router/cart.router.js
@@ -0,0 +1,67 @@
+import { Router } from "express"
+import cartManager from "../managers/cartManager.js"
+import productManager from "../managers/productManager.js"
+
+
+const router = Router()
+
+//Crear carrito
+router.post("/carts", async(req, res) =>{
+    try{
+        const cart = await cartManager.createCart()
+        res.status(201).json({ status:"ok", cart})
+
+    }catch(err){
+        console.log(err)
+        res.status(500).json({ status:"error", msg:"Error interno del servidor"})
+    }
+})
+
+//obtener todos los carritos
+router.get("/carts", async(req, res) =>{
+    const carts = await cartManager.getCarts()
+    res.send(carts)
+})
+
+//obtener un carrito
+router.get("/carts/:cid", async(req, res) =>{
+    try{
+        const { cid } = req.params
+        const cart = await cartManager.getCartById(cid)
+
+        if(!cart) return res.status(404).json({ status:"error", msg:"Carrito no encontrado"})
+
+        res.status(200).json({ status:"ok", cart})
+
+    }catch(err){
+        console.log(err)
+        res.status(500).json({ status:"error", msg:"Error interno del servidor"})
+    }
+})
+
+
+//agregar producto al carrito
+router.post("/carts/:cid/product/:pid", async(req, res) =>{
+    try{
+        const { cid, pid } = req.params
+
+        //verificar si el carrito existe
+        const ExistCart = await cartManager.getCartById(cid)
+        if(!ExistCart) return res.status(404).json({ status:"error", msg:"Carrito no encontrado"})
+
+        //verificar si el producto existe
+        const ExistProduct = await productManager.getProductById(pid)
+        if(!ExistProduct) return res.status(404).json({ status:"error", msg:"Producto no encontrado"})
+
+        //agregar producto al carrito
+        const cart = await cartManager.addProductToCart(cid, pid)
+
+        res.status(201).json({ status:"ok", cart})
+    }catch(err){
+        console.log(err)
+        res.status(500).json({ status:"error", msg:"Error interno del servidor"})
+    }
+})
+
+
+export default router
