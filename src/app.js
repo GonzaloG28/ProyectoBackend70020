14c7c0c45ed8e56919a50713e044b965fbf2d07f
GonzaloG28 <gonzalogimenez280@gmail.com>
2024-07-09 15:45:23 -0300

    implementamos handlebar y socket.io al proyecto

--- src/app.js
+++ src/app.js
@@ -1,21 +1,40 @@
 import express from "express"
 import productRouter from "./router/product.router.js"
 import cartRouter from "./router/cart.router.js"
-
+import handlebars from "express-handlebars"
+import { Server } from "socket.io"
+import __dirname from "./dirname.js"
+import viewsRoutes from "./router/views.routes.js"
 
 const PORT = 8080
 const app = express()
-
+ 
 app.use(express.json())
 //sirve para que pueda leer todo tipo de escritura
 app.use(express.urlencoded({extended: true}))
+
+app.engine("handlebars", handlebars.engine()) // inicia el motor de la plantilla
+app.set("views", __dirname + "/views") // Indicamos que ruta se encuentran las vistas
+app.set("view engine", "handlebars") // indicamos con que motor vamos a actualizar las vistas
+
+
 //archivos publicos
 app.use(express.static("public"))
 
 
 app.use("/api", productRouter)
 app.use("/api", cartRouter)
+app.use("/", viewsRoutes);
 
-app.listen(PORT, () =>{
+
+
+ const serverHttp = app.listen(PORT, () =>{
     console.log(`Servidor escuchando en el puerto ${PORT}`)
+})
+
+
+export const io = new Server(serverHttp)
+
+io.on("connection", (socket) =>{
+    console.log("Nuevo cliente conectado")
 })
---
+++ src/router/views.routes.js
@@ -0,0 +1,7 @@
+import { Router } from "express"
+import productManager from "../managers/productManager.js"
+import { io } from "../app.js"
+
+const router = Router()
+
+export default router
