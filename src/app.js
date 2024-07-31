47e4789ca8b21c86ff6ddb1f807b844748deda66
GonzaloG28 <gonzalogimenez280@gmail.com>
2024-07-31 11:58:13 -0400

    PreEntrega final

--- src/app.js
+++ src/app.js
@@ -4,12 +4,7 @@ import cartRouter from "./router/cart.router.js"
 import { connectMongoDB } from "./config/mongoDB.config.js"
 import envs from "./config/envs.config.js"
 
-//import handlebars from "express-handlebars"
-// import { Server } from "socket.io"
-//import __dirname from "./dirname.js"
-//import viewsRoutes from "./router/views.routes.js"
 
-//const PORT = 8080
 const app = express()
 
 connectMongoDB()
@@ -18,10 +13,6 @@ app.use(express.json())
 //sirve para que pueda leer todo tipo de escritura
 app.use(express.urlencoded({extended: true}))
 
-//app.engine("handlebars", handlebars.engine()) // inicia el motor de la plantilla
-//app.set("views", __dirname + "/views") // Indicamos que ruta se encuentran las vistas
-//app.set("view engine", "handlebars") // indicamos con que motor vamos a actualizar las vistas
-
 
 //archivos publicos
 app.use(express.static("public"))
@@ -29,22 +20,8 @@ app.use(express.static("public"))
 //rutas
 app.use("/api", productRouter) 
 app.use("/api", cartRouter)
-//app.use("/", viewsRoutes)
-
 
 app.listen(envs.PORT, () => {
     console.log(`Servidor escuchando en el puerto ${envs.PORT}`)
 })
-
-//servidor conectado en puerto 8080
-/*  const serverHttp = app.listen(PORT, () =>{
-    console.log(`Servidor escuchando en el puerto ${PORT}`)
-})
-
-
-export const io = new Server(serverHttp)
-
-io.on("connection", (socket) =>{
-    console.log("Nuevo cliente conectado")
-}) */
 
